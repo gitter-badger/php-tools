2a714ebc6a56147347b3dd2df26ee2010ab27dc5
decima <h.larget@gmail.com>
2014-07-17 23:18:43 +0200

    debug

--- example.php
+++ example.php
@@ -1,9 +1,11 @@
 <?php
 require './example_includes.php';
 $username = "bob";
+$name="decima";
 t("hello my friend!");
 echo "\n";
 t("your name is %name", array("%name"=>$username));
 echo "\n";
 t("my name is %name and i'm %age year old", array("%name"=>$name,"%age"=>23));
+echo "\n";
 
