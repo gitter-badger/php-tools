d55cfeadb91e900a2dd2291cbec348117e753a17
decima <h.larget@gmail.com>
2014-07-17 23:20:29 +0200

    translatings tests

---
+++ translate/fr.php
@@ -0,0 +1,5 @@
+<?php
+$translates = array();
+$translates["3671f241767f9ca0b9a775600706b247"]="Bonjour mon ami!";
+$translates["09e027c37dd9158aeaa1212b54ea3e63"]="ton nom est %name";
+$translates["094bfb0a6b9947a69fadf4db436e6b73"]="je m'appelle %name et j'ai %age ans";
