170c22128eb72dd6456084e9adb40268e92edf63
decima <h.larget@gmail.com>
2014-08-24 16:21:09 +0300

    examples for new version.

--- router/example.php
+++ router/example.php
@@ -11,22 +11,28 @@ function err_not_found_404() {
 $route = new Router();
 /* Assign static routes. */
 $route->assign("/", function() {
-    echo "this is home page.";
+    return "this is home page.";
 });
-$route->assign("/about", function() {
-    echo "this is the about page.";
-});
-/* Assign variables routes. */
-$route->assign("/user/{user_id}", function($uid) {
-    echo "user $uid profile";
-});
+/* Assign to file */
+$route->assign("/about", "about.html");
+/* Assign to text */
+$route->assign("/about/goods", "About goods");
+
+
+
+/*
+ *  Assign variables routes. 
+ */
+$route->assign("/user/{user_id}", "profile.html");
 
 $route->assign("/give/{user_id}/{object}", function($uid, $object) {
-    echo "give $object to user $uid";
+    //Remove object from current user to next user.
+    return "give $object to user $uid";
 });
 
+
 try {
-    $route->proceed();
+    echo $route->proceed();
 } catch (Err_not_found_404 $e) {
     err_not_found_404();
 }
